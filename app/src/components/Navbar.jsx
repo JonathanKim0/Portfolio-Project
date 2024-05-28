defb660ec1a06004a6c6ed25bf0d757be47b3c4d
JonathanKim0 <89949938+JonathanKim0@users.noreply.github.com>
2024-05-28 04:04:09 -0400

    functional navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,34 +1,17 @@
 import React, { useState } from 'react'
-import Logo from '../assets/img/penguin4k.jpg'
 import { Link } from 'react-router-dom'
 import '../assets/css/Navbar.css'
-import ReorderIcon from '@mui/icons-material/Reorder';
 function Navbar() {
-
-  const [visibleLinks, setVisibleLinks] = useState(false);
-
-  const toggleNavBar = () => {
-    setVisibleLinks(!visibleLinks);
-  }
 
   return (
     <div className='navbar'>
-        <div className='leftSide' id={visibleLinks ? "visible" : "hidden"}>
-          <Link to='/home'><img src={Logo}/></Link>
-          <div className='hiddenLinks'>
-            <Link to='/home'>Home</Link>
-            <Link to='/project'>Project</Link>
-            <Link to='/contact'>Contact</Link>
-          </div>
-        </div>
-        <div className='rightSide'>
-            <Link to='/home'>Home</Link>
-            <Link to='/project'>Project</Link>
-            <Link to='/contact'>Contact</Link>
-            <button onClick={toggleNavBar}>
-              <ReorderIcon />
-            </button>
-        </div>
+      <Link to='/home'>Home</Link>
+      <p>|</p>
+      <Link to='/about'>About</Link>
+      <p>|</p>
+      <Link to='/project'>Projects</Link>
+      <p>|</p>
+      <Link to='/contact'>Contact</Link>
     </div>
   )
 }
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import HomeBackground from "../assets/img/jett-background.jpg"
 import "../assets/css/Home.css"
 
 function Home() {
   return (
     <div className='home'>
-      <div className='headerContainer' style={{backgroundImage: `url(${HomeBackground})`}}>
-        <h1> My Website </h1>
-        <p> Here is my website! </p>
-      </div>
+      <h1> My Website </h1>
+      <p> Here is my website! </p>
     </div>
   )
 }
