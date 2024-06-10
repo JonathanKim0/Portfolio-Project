34ad177f7cc494830782277b0b06859007435814
JonathanKim0 <89949938+JonathanKim0@users.noreply.github.com>
2024-06-10 03:21:46 -0400

    about me + home

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../assets/css/Home.css'
+import winnieImage from '../assets/img/winnie_wink.jpg';
 
 function Header() {
   return (
     <div id='home-div'>
-        HOME
+        <p id='home-p'>Hello<br/>I'm Jonathan</p>
+        <img id='portrait' src={winnieImage}></img>
     </div>
   )
 }
--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import '../assets/css/Navbar.css'
 
 function Navbar() {
